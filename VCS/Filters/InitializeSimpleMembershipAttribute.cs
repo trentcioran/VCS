f6075e6cf0c77fe563ded7d475dc5918b3f8e57b
trentcioran <trentcioran@gmail.com>
2012-12-11 00:26:15 -0600

    remove oauth support, use basic auth
    removed oauth models, views and controller methods to clean up code

--- VCS/Filters/InitializeSimpleMembershipAttribute.cs
+++ VCS/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,11 +38,13 @@ namespace VCS.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", 
+                        autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
-                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. " +
+                                                        "For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
                 }
             }
         }
---
+++ VCS/Models/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace VCS.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
